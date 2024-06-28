3433443d364029287c1f9cc5cc18c373c853f302
ma20ria <itsmaria1804@gmail.com>
2024-06-28 11:10:24 +0530

    Login Done

--- app/src/main/java/com/mariamanuel/entrylog/MainActivity.java
+++ app/src/main/java/com/mariamanuel/entrylog/MainActivity.java
@@ -1,20 +1,35 @@
 package com.mariamanuel.entrylog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText e1,e2;
+AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+e1=(EditText) findViewById(R.id.uname);
+e2=(EditText) findViewById(R.id.pass);
+b1=(AppCompatButton) findViewById(R.id.lobtn);
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String getUsername=e1.getText().toString();
+        String getPassword=e2.getText().toString();
+        Toast.makeText(getApplicationContext(), getUsername+" "+getPassword, Toast.LENGTH_SHORT).show();
+    }
+});
     }
 }
